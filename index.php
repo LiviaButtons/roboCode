a77414337c1fea0fee746deae59fcce528c9140d
Sophie Bonnet <s.bonnet@interface3.be>
2018-11-29 17:00:27 +0100

    barre progress à 0 dans index

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
            
 <!--              progress bar   -->
                   <div class="progress progresJeu" style="position: relative">
-                    <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">20% Complete
+                    <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%; color:#605959;">0% Complete
                     </div>
                   </div>
              
